64142c912807d4951605430121405850403d3f4f
VladMiryaha <vlad.miryaha@gmail.com>
2016-10-02 03:12:45 +0300

    не засчитано. в обоих упражнения ошибки на уровне идеи решения

--- launch_arg.c
+++ launch_arg.c
@@ -5,10 +5,18 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/*
+ * Совсем неверный подход к решению.
+ * Никто не гарантирует, что у вас число аргументов при запуске будет не больше двух.
+ * Мы специально писали ф-ю Split, которая строку разбила бы на отдельные токены, количество которых может быть произвольным.
+ */
 int main()
 {
 	FILE *file = fopen("input.txt", "r"); 
 	int countLines = 0, seconds = 0, status; 
+/*
+ * магических чисел вроде 10 быть не должно. объявляйте константы.
+ */
 	char s1[10], s2[10], s3[10], s4[10]; 
 
 	fscanf(file, "%s", s1); 
@@ -18,14 +26,19 @@ int main()
 	countLines = atoi(s1);
 	seconds = atoi(s4);
 	pid_t pid1 = fork(); 
+/*
+ * Непонятна цель этого wait
+ */
 	wait(&status); 
 	if (pid1 == 0) 
 	{ 
 		sleep(seconds); 
 		execlp(s2, s2, s3, NULL); 
-	}  
+	}
 
- 
+  /*
+   * ваш код написан для одно конкретного файла запуска. в общем случае работать не будет.
+   */
 	fscanf(file, "%s ", s1); 
 	fscanf(file, "%s", s2);
 	seconds = atoi(s2);
