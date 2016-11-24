ce3b45521038cf035009772e85dced1eefdfe4c7
Egor Brigida <student.brigida@gmail.com>
2016-11-24 20:10:32 +0300

    Search for the file

---
+++ FindDir.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int i = 0;
+void Find(char* path, int depth, char* fileName)
+{
+	if(depth <= 0)
+		return;
+	struct stat buf;
+	DIR* d = opendir(path);
+	struct dirent *dir;
+	while(1)
+	{
+		dir = readdir(d);
+		if (dir != NULL)
+		{
+			stat(dir->d_name, &buf);
+			if (dir->d_type == 4)
+			{
+					if((strcmp(dir->d_name, ".") != 0) && (strcmp(dir->d_name, "..") != 0))
+					{	
+						
+						char s[1000];
+						strcpy(s, path);
+						strcat(s, "/");
+						strcat(s, dir->d_name);
+						Find(s, depth - 1, fileName);
+					}
+			}
+			else
+			{
+				if(!strcmp(dir->d_name, fileName))
+				{
+					printf("%s/%s\n", path, dir->d_name);
+				}
+			}	
+		}
+		else
+		{
+			break;
+		}
+	}
+	closedir(d);
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	Find("..", 2, "wiper.txt");
+	return 0;
+}
+
