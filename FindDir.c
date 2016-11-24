b18acbed4d0e5fd6b12b49b03cfe10f455a8a44d
VladMiryaha <vlad.miryaha@gmail.com>
2016-11-24 23:55:43 +0300

    fixit

--- FindDir.c
+++ FindDir.c
@@ -18,12 +18,20 @@ void Find(char* path, int depth, char* fileName)
 		dir = readdir(d);
 		if (dir != NULL)
 		{
+      /*
+       * FIXIT:
+       * Мы же обсуждали с вами в самом конце семирара, что передаёте в stat неверный первый аргумент.
+       * Вы передаете только имя, а нужно путь до этого файла и директории тоже указывать.
+       */
 			stat(dir->d_name, &buf);
 			if (dir->d_type == 4)
 			{
 					if((strcmp(dir->d_name, ".") != 0) && (strcmp(dir->d_name, "..") != 0))
 					{	
-						
+						/*
+             * FIXIT:
+             * Нужно завести отдельную константу для 1000.
+             */
 						char s[1000];
 						strcpy(s, path);
 						strcat(s, "/");
